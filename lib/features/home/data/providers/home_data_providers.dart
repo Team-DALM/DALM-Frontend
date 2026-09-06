9808b1ba5a83cfa08e177ed1aeb65aa140c22cd6
Shin Han Seo <imkara1@naver.com>
2026-09-07 00:39:09 +0900

    fix: 변수명 수정 (#14)

--- lib/features/home/data/providers/home_data_providers.dart
+++ lib/features/home/data/providers/home_data_providers.dart
@@ -5,14 +5,14 @@ import '../../domain/repositories/home_repository.dart';
 import '../datasources/home_remote_data_source.dart';
 import '../repositories/home_repository_impl.dart';
 
-final HomeRemoteDataSourceProvider = Provider<HomeRemoteDataSource>((ref) {
+final homeRemoteDataSourceProvider = Provider<HomeRemoteDataSource>((ref) {
   final dio = ref.watch(dioProvider);
 
   return DioHomeRemoteDataSource(dio);
 });
 
 final homeRepositoryProvider = Provider<HomeRepository>((ref) {
-  final remoteDataSource = ref.watch(HomeRemoteDataSourceProvider);
+  final remoteDataSource = ref.watch(homeRemoteDataSourceProvider);
 
   return HomeRepositoryImpl(remoteDataSource);
 });
